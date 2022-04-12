c8feeedf73f1d173f6ff43077aa30a06d847b18a
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-04-11 20:40:39 -0700

    defined interval, added in conditionals, and onclick button for starting the timer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import { useEffect, useState } from "react";
-
+import './App.css';
 
 const App = () => {
   const [time, setTime] = useState(0)
   const [start, setStart] = useState(false)
 
+useEffect(() => {
+  let interval = null;
 
-  
+  if (start) {
+    interval = setInterval(() => {
+      setTime(prevTime => prevTime + 10)
+    }, 10) 
+  } else {
+    clearInterval(interval)
+  }
+  return () => clearInterval(interval)
+}, [start])
+
   return (
     <div className="App">
       <h1>StopWatch</h1>
+      <h1>
+        <spam>{time}</spam>
+      </h1>
+      <div>
+        <button onClick={() => setStart(true)}>Start</button>
+      </div>
     </div>
   )
 }
