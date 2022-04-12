a43224308b7552c2dd8dbbc3f917e623b055c78e
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-04-11 20:47:26 -0700

    added a format for displaying the counting

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ const App = () => {
   const [time, setTime] = useState(0)
   const [start, setStart] = useState(false)
 
-useEffect(() => {
-  let interval = null;
+  useEffect(() => {
+    let interval = null;
 
-  if (start) {
-    interval = setInterval(() => {
-      setTime(prevTime => prevTime + 10)
-    }, 10) 
-  } else {
-    clearInterval(interval)
-  }
-  return () => clearInterval(interval)
-}, [start])
+    if (start) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+      }, 10)
+    } else {
+      clearInterval(interval)
+    }
+    return () => clearInterval(interval)
+  }, [start])
 
   return (
     <div className="App">
       <h1>StopWatch</h1>
       <h1>
-        <spam>{time}</spam>
+        <spam>{("0" + (time / 10) % 1000).slice(-2)}</spam>
       </h1>
       <div>
         <button onClick={() => setStart(true)}>Start</button>
